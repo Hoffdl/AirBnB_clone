982c3a3a34c52b228a8d754abd8df226151eae6d
Henry <ikegwuonuhenry25@gmail.com>
2023-05-14 13:30:57 +0000

    Correct

--- models/base_model.py
+++ models/base_model.py
@@ -1,64 +1,112 @@
 #!/usr/bin/python3
-"""base_model module defines the BaseModel Class """
+"""
+This File defines the BaseModel class that will
+serve as the base class for all our models."""
 
-from datetime import datetime
 from uuid import uuid4
+from datetime import datetime
 import models
 
 
 class BaseModel:
-    """BaseModel defines all common attributes/methods for other classes"""
+    """Base class for all our classes"""
 
     def __init__(self, *args, **kwargs):
-        """Instantializes the class BaseModel
-        Args:
-            args: args not to be used
-            kwargs: key/value arg (dictionary) to instantiate the class
-        Attr:
-            id: (str) unique ID
-            created_at: date and time of creation
-            updated_at: update time
-        """
+        """constructor it either deserialize
+        a serialized class or intialize a new"""
 
-        if kwargs != {}:
-            for key, val in kwargs.items():
-                if key != "__class__":
-                    if key == 'created_at' or key == 'updated_at':
-                        value = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
-                        setattr(self, key, value)
-                    else:
-                        setattr(self, key, val)
-        else:
+        # initialize if nothing is passed
+        if kwargs == {}:
             self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
             models.storage.new(self)
+            return
+
+        # using key words (deserialize)
+        for key, val in kwargs.items():
+            if key == '__class__':
+                continue
+            self.__dict__[key] = val
+        if 'created_at' in kwargs:
+            self.created_at = datetime.strptime(
+                    kwargs['created_at'],
+                    '%Y-%m-%dT%H:%M:%S.%f')
+        if 'updated_at' in kwargs:
+            self.updated_at = datetime.strptime(
+                    kwargs['updated_at'],
+                    '%Y-%m-%dT%H:%M:%S.%f')
 
     def __str__(self):
-        """Return the string documentation of an instance"""
-
-        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+        """overide str representation of self"""
+        fmt = "[{}] ({}) {}"
+        return fmt.format(
+                type(self).__name__,
+                self.id,
+                self.__dict__)
 
     def save(self):
-        """saves the current time into the attr updated_at"""
-
-        self.updated_at = datetime.now()
+        """updates last updated variable"""
+        self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values\
-                (__dict__ of an instance)"""
+        """Returns a dictionary representation of self"""
+        temp = {**self.__dict__}
+        temp['__class__'] = type(self).__name__
+        temp['created_at'] = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        temp['updated_at'] = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        return temp
 
-        dic = dict(self.__dict__)
-        dic['__class__'] = type(self).__name__
-        dic['id'] = self.id
-        if type(self.updated_at) is str:
-            dic['updated_at'] = self.updated_at
-        else:
-            dic['updated_at'] = self.updated_at.isoformat()
-        if type(self.created_at) is str:
-            dic['created_at'] = dic['created_at']
-        else:
-            dic['created_at'] = dic['created_at'].isoformat()
+    @classmethod
+    def all(cls):
+        """Retrieve all current instances of cls"""
+        return models.storage.find_all(cls.__name__)
 
-        return dic
+    @classmethod
+    def count(cls):
+        """Get the number of all current instances of cls"""
+        return len(models.storage.find_all(cls.__name__))
+
+    @classmethod
+    def create(cls, *args, **kwargs):
+        """Creates an Instance"""
+        new = cls(*args, **kwargs)
+        return new.id
+
+    @classmethod
+    def show(cls, instance_id):
+        """Retrieve an instance"""
+        return models.storage.find_by_id(
+            cls.__name__,
+            instance_id
+        )
+
+    @classmethod
+    def destroy(cls, instance_id):
+        """Deletes an instance"""
+        return models.storage.delete_by_id(
+            cls.__name__,
+            instance_id
+        )
+
+    @classmethod
+    def update(cls, instance_id, *args):
+        """Updates an instance
+        if args has one elem and its a dict:
+        it updates by key value
+        else:
+        updates by first being key and second being value"""
+        if not len(args):
+            print("** attribute name missing **")
+            return
+        if len(args) == 1 and isinstance(args[0], dict):
+            args = args[0].items()
+        else:
+            args = [args[:2]]
+        for arg in args:
+            models.storage.update_one(
+                cls.__name__,
+                instance_id,
+                *arg
+            )
